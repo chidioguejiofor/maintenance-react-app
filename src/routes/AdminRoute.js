238bc9589163e0651b45eea1d96120803c8fa75e
Chidiebere Oguejiofor <oguejiofor.chidiebere@andela.com>
2018-10-08 11:19:57 +0100

    feat(create-admin-dashboard): admin dashboard
    - adds an AdminRoute to the routes folder
    
    [#161050287]

---
+++ src/routes/AdminRoute.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute';
+
+/**
+ *this checks if the logged in user is the admin
+ * @returns {boolean} true when the logged in user is an admin
+ */
+export const isAdmin = () => localStorage.admin;
+
+const AdminRoute = ({ component: Component, ...rest }) => (
+  <ProtectedRoute
+    {...rest}
+    render={props => (isAdmin()
+      ? <Component {...props} /> : <Redirect to="/admin/login" />)}
+  />
+);
+
+
+export default AdminRoute;
