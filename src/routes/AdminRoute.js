f20d52dc46c57b07add2ee03e18a563311266d03
Chidiebere Ogujeiofor <38565349+chidioguejiofor@users.noreply.github.com>
2018-10-09 01:53:39 +0100

    Revert "corrected hound"
    This reverts commit 7bc6f9b353943e27a7e560b628c7ed2cfdfbf525.

--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -1,19 +1,20 @@
 
-import PropTypes from 'prop-types';
-import routesHelper from '../helpers/routesHelper';
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
 /**
  *this checks if the logged in user is the admin
  * @returns {boolean} true when the logged in user is an admin
  */
-export const isAdmin = () => !!(
-  localStorage.admin === 'true' && localStorage.token);
+export const isAdmin = () => !!(localStorage.admin && localStorage.token);
+
+const AdminRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (isAdmin() ? (<Component {...props} />)
+      : (<Redirect to="/" />))}
+  />
+);
 
 
-const AdminRoute = ({ component: Component,
-  ...rest }) => routesHelper(isAdmin(), Component, rest, '/');
-
-
-AdminRoute.propTypes = {
-  component: PropTypes.element.isRequired,
-};
 export default AdminRoute;
